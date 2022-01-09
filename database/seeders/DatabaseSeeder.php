6d72734ec942845988460784692df1b3244bf333
Slava Mora <gitmooncat@gmail.com>
2022-01-09 12:39:47 +0200

    ch dataseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,14 +16,61 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::table('countries')->insert([
-            'country_name' => 'GR',
+            'id' => 1,
+            'country_name' => 'USE',
+        ]);
+        DB::table('countries')->insert([
+            'id' => 2,
+            'country_name' => 'China',
+        ]);
+        DB::table('countries')->insert([
+            'id' => 3,
+            'country_name' => 'Ukraine',
+        ]);
+        DB::table('countries')->insert([
+            'id' => 4,
+            'country_name' => 'Russia',
+        ]);
+        DB::table('countries')->insert([
+            'id' => 5,
+            'country_name' => 'Germany',
+        ]);
+        DB::table('countries')->insert([
+            'id' => 6,
+            'country_name' => 'England',
         ]);
 
+        //
         DB::table('companies')->insert([
-            'company_name' => 'bwt',
+            'company_name' => 'bwt1',
             'country_id' => 1,
         ]);
 
+        DB::table('companies')->insert([
+            'company_name' => 'bwt2',
+            'country_id' => 1,
+        ]);
+
+        DB::table('companies')->insert([
+            'company_name' => 'bwt3',
+            'country_id' => 1,
+        ]);
+
+        //
+        DB::table('companies')->insert([
+            'company_name' => 'bwt4',
+            'country_id' => 2,
+        ]);
+
+        DB::table('companies')->insert([
+            'company_name' => 'bwt5',
+            'country_id' => 2,
+        ]);
+
+        DB::table('companies')->insert([
+            'company_name' => 'bwt6',
+            'country_id' => 2,
+        ]);
         DB::table('users')->insert([
             'user_name' => 'user1',
         ]);
